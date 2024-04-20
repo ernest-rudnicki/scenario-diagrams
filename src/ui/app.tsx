73933568f2b752ff530186da22b209d17c14a13a
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2024-04-20 11:29:14 +0200

    add elements

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -21,6 +21,37 @@ const AppComponent = () => {
             <div class="h-full">
                 <div id="paper"></div>
             </div>
+            <div 
+                class="absolute bg-slate-500 h-full right-0 border-l-2 border-solid border-slate-950 p-2 overflow-auto" 
+                style="width: 200px">
+                <div data-diagram="character" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Character</div>
+                </div>
+                <div data-diagram="npc" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">NPC</div>
+                </div>
+                <div data-diagram="enemy" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Enemy</div>
+                </div>
+                <div data-diagram="location" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Location</div>
+                </div>
+                <div data-diagram="action" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Action</div>
+                </div>
+                <div data-diagram="attribute" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Attribute</div>
+                </div>
+                <div data-diagram="building" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Building</div>
+                </div>
+                <div data-diagram="container" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Container</div>
+                </div>
+                <div data-diagram="item" class="border-2 border-solid border-slate-950 flex justify-center items-center py-4 mb-2">
+                    <div class="text-slate-50">Item</div>
+                </div>
+            </div>
             <BottomBar onSubmit={onSubmit} />
         </main>
     )
